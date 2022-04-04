295df40353c08040a68947f0c61a90d5bbfe861d
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-04-04 22:58:25 +0600

    Home Page Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css'
 import carImg from  '../../resources/images/home-car.png'
+import { Button } from "react-bootstrap";
 
 
 
@@ -21,8 +22,11 @@ const Home = () => {
               roads.Impressive Desing will make the diffeence.
             </p>
             <h2 className="text-orange">6 Free Services</h2>
-            <p>Mazda CX-5 users can have 6 free services and 3 services @50% off at Mazda center.Register below to avail this offer.</p>
-            <button className='btn'>Register Now</button>
+            <p>
+              Mazda CX-5 users can have 6 free services and 3 services @50% off
+              at Mazda center.Register below to avail this offer.
+            </p>
+            <Button variant="outline-info">Register Now</Button>
           </div>
           <div className="img-section">
             <img src={carImg} alt="car-overview" />
